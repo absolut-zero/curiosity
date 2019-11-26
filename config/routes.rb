4e752494e17fa3d00854a61dee9628981d09542c
Ameya Akhouri <ameya.akhouri@gmail.com>
2019-11-26 16:59:32 +1100

    Add routes for concepts

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :documents, only: [:index, :show, :create, :update]
+  resources :documents, only: [:index, :show, :create, :update] do
+    :concepts, only: [:index, :create, :update]
+  end
 end
