eb33b79cb02a43c081a47e9e2de0b4d7c932e444
Sophie Lejeune <sophie.shadi.lejeune@gmail.com>
2019-11-26 17:16:43 +1100

    added a delete method for the index and show page of the documents

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :documents, only: [:index, :show, :create, :update]
+  resources :documents, only: [:index, :show, :create, :update, :destroy]
 end
