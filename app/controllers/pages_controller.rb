81a209b02faf2357449c144439378f44364185f0
Rexmortus the Red <rexmortus@Perihelion.local>
2019-12-05 12:27:00 +1100

    Redirect pages#home to '/user/sign_in'

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,15 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @documents_all = Document.where(user: current_user)
-    @drafts = @documents_all.select { |doc| doc.concepts.length.zero? }
+    if user_signed_in?
+      @documents_all = Document.where(user: current_user)
+      @drafts = @documents_all.select { |doc| doc.concepts.length.zero? }
 
-    all_revision_sessions_with_date = RevisionSession.where(user: current_user).where("scheduled_at <= ?", Date.today.at_beginning_of_day).where(completed_at: nil).order(scheduled_at: :asc)
-    @revision_sessions = all_revision_sessions_with_date.select { |session| !session.revision_session_concepts.length.zero? }
+      all_revision_sessions_with_date = RevisionSession.where(user: current_user).where("scheduled_at <= ?", Date.today.at_beginning_of_day).where(completed_at: nil).order(scheduled_at: :asc)
+      @revision_sessions = all_revision_sessions_with_date.select { |session| !session.revision_session_concepts.length.zero? }
+    else
+      redirect_to "/users/sign_in"
+    end
   end
 
   def archive
